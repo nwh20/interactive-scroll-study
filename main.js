def20fd520f8afce908db7ed112789edcd28792c
nwh20 <hnamwon@gmail.com>
2020-10-27 15:16:29 +0000

    third

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 (() => {
+    const actions = {
+        animation1 (key) {
+            if (key) {
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(${window.innerWidth}px)`;
+            } else {
+                document.querySelector('[data-index="2"] .bird').style.transform = `translateX(-100%)`;
+            }
+        },
+        animation2 (key) {
+            if (key) {
+                document.querySelector('[data-index="5"] .bird').style.transform = `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)`;
+            } else {
+                document.querySelector('[data-index="5"] .bird').style.transform = `translateX(-100%)`;
+            }
+        }
+    }
+
     const stepElems = document.querySelectorAll('.step');
     const graphicElems = document.querySelectorAll('.graphic-item');
 
@@ -16,12 +33,18 @@
         graphicElems[i].setAttribute('data-index', i);
     }
 
-    const activate = () => {
+    const activate = (action) => {
         currentItem.classList.add('visible');
+        if (action) {
+            actions[action](true);
+        }
     }
 
-    const inactivate = () => {
+    const inactivate = (action) => {
         currentItem.classList.remove('visible');
+        if (action) {
+            actions[action](false);
+        }
     }
 
     window.addEventListener('scroll', () => {
@@ -35,12 +58,18 @@
             boundingRect = step.getBoundingClientRect();
 
             if (boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8) {
-                inactivate();
+                inactivate(currentItem.dataset.action);
                 currentItem = graphicElems[step.dataset.index];
-                activate();
+                activate(currentItem.dataset.action);
             }
         }
     });
+
+    window.addEventListener('load', () => {
+        setTimeout(() => {
+            scrollTo(0, 0);
+        }, 100)
+    })
 
     activate();
 
